463c081c654f34f9d9c04e010814cd1e8f2ed7f6
kasiausuaj <andrejlebed120@gmail.com>
2024-01-16 15:41:33 +0700

    fifth commit

--- vue/main.js
+++ vue/main.js
@@ -7,19 +7,38 @@ let app = new Vue({
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         linkText: "More products like this",
-        onSale: true,
+        inStock: true,
         inventory: 100,
-        detail: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "/assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "/assets/vmSocks-blue-onWhite.jpg"
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-    }
+        cart: 0,
+
+
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+        removeFromCart() {
+            this.cart -= 1
+        }
+
+
+
+     }
 })
